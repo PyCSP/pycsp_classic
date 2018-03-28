c025afc39fe80b2d918f0faa6df59e77af6da35d
John Markus Bjørndalen <jmb@cs.uit.no>
2018-03-28 15:03:21 +0200

    checking memory consumption for n simple processes

---
+++ test/n_procs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+from common import *
+from pycsp import *
+from pycsp.plugNplay import *
+import os
+import psutil
+import sys
+import time
+
+N_PROCS = 10 if len(sys.argv) < 2 else int(sys.argv[1])
+
+@process
+def simple_proc(pid, checkin, cin):
+    # check in
+    checkin(pid)
+    # wait for poison
+    while True:
+        x = cin()
+
+@process
+def killer(chin, pch, nprocs):
+    print("Killer waiting for the other procs to call in")
+    for i in range(nprocs):
+        x = chin()
+    print("Done, checking memory usage")
+    p = psutil.Process(os.getpid())
+    rss = p.memory_info().rss
+    print(f"RSS now {rss}  {rss/(1024**2)}M")
+    print("now poisioning")
+    pch.poison()
+
+    
+def run_n_procs(n):
+    print(f"Running with {n} simple_procs")
+    ch = Any2OneChannel()
+    pch =  One2AnyChannel()
+    t1 = time.time()
+    tasks = [simple_proc(i, ch.write, pch.read) for i in range(N_PROCS)]
+    tasks.append(killer(ch.read, pch, n))
+    t2 = time.time()
+    Parallel(*tasks)
+    t3 = time.time()
+    print("Creating tasks: {:10.3f} us".format(1_000_000 * (t2-t1)))
+    print("Running  tasks: {:10.3f} us".format(1_000_000 * (t3-t2)))
+
+run_n_procs(N_PROCS)
+    
